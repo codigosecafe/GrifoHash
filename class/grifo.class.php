ca737e6a471c5a45af554c1daccd1d8d2e0a1e5d
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2017-05-26 02:45:12 -0300

    [Corregindo descrição no arquivo grifo.class.php]

--- class/grifo.class.php
+++ class/grifo.class.php
@@ -5,7 +5,7 @@
  * A classe Grifo tem esse nome como sinonimo da palavra enigma inspirado no filme **O Jogo da Imitação**. 
  * Com uma ideia bem simples, de juntar alguns metodos de codificação e geração de hash para tornar a senha 
  * um pouco mais segura do que utilizar apenas um método como MD5, SHA-1 SHA-256 ou SHA-512. 
- * Esse métodos cumprem o que promete mas são notavelmente inseguros. 
+ * Esses métodos cumprem o que promete mas são notavelmente inseguros. 
  * A classe Grifo além de usuflir de alguns desses métodos finaliza o hashing com o padrão  bcrypt/blowfish.  
  * Recomendo a leitura do artigo em inglês: [https://en.wikipedia.org/wiki/Bcrypt](https://en.wikipedia.org/wiki/Bcrypt)
  * 
