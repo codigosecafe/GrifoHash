1e1d2316fc8865fc3097b2952da8825a3539ce5a
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2020-04-27 19:10:18 -0300

    UPD

--- src/GrifoHash/GrifoHash.php
+++ src/GrifoHash/GrifoHash.php
@@ -1,14 +1,16 @@
 <?php
 namespace codigosecafe;
+
+use Exception;
 /**
  * Grifo é uma classe PHP para gerar hashing de senha e validação.
- * A classe Grifo tem esse nome como sinonimo da palavra enigma inspirado no filme **O Jogo da Imitação**. 
- * Com uma ideia bem simples, de juntar alguns metodos de codificação e geração de hash para tornar a senha 
- * um pouco mais segura do que utilizar apenas um método como MD5, SHA-1 SHA-256 ou SHA-512. 
- * Esses métodos cumprem o que promete mas são notavelmente inseguros. 
- * A classe Grifo além de usuflir de alguns desses métodos finaliza o hashing com o padrão  bcrypt/blowfish.  
+ * A classe Grifo tem esse nome como sinonimo da palavra enigma inspirado no filme **O Jogo da Imitação**.
+ * Com uma ideia bem simples, de juntar alguns metodos de codificação e geração de hash para tornar a senha
+ * um pouco mais segura do que utilizar apenas um método como MD5, SHA-1 SHA-256 ou SHA-512.
+ * Esses métodos cumprem o que promete mas são notavelmente inseguros.
+ * A classe Grifo além de usuflir de alguns desses métodos finaliza o hashing com o padrão  bcrypt/blowfish.
  * Recomendo a leitura do artigo em inglês: [https://en.wikipedia.org/wiki/Bcrypt](https://en.wikipedia.org/wiki/Bcrypt)
- * 
+ *
  * @author Claudio Alexssandro Lino <claudio@codigosecafe.com.br>
  * @link   https://github.com/codigosecafe/grifo-hash-php
  * @date 21/01/2018
@@ -18,7 +20,7 @@ class GrifoHash
 
 	protected static $Prefix_salt = '2a'; // isso habilita o blowfish para que seja usado
 	protected static $Cost_default = '12';
-	
+
 	protected static $_token;
 
     /**
@@ -26,14 +28,20 @@ class GrifoHash
      */
     public function __construct()
     {
+        try {
 
-        $tokenBase = self::hashBase64($_SERVER ['REQUEST_URI']);
+            $tokenBase = self::hashBase64(env('APP_KEY'));
+
+        } catch (Exception $event) {
+            $tokenBase = self::hashBase64('codigosecafe');
+        }
+
     	self::$_token = crypt( $tokenBase , self::$Prefix_salt );
 
     }
     /**
-    * Método responsável por gerar o hashing da senha que será armazenada no banco de dados. 
-    * Indenpedente da senha sempre será gerado um hashing unico. Bom lembrar que o hash sempre será composto de 60 caracteres, 
+    * Método responsável por gerar o hashing da senha que será armazenada no banco de dados.
+    * Indenpedente da senha sempre será gerado um hashing unico. Bom lembrar que o hash sempre será composto de 60 caracteres,
     * então você pode definir a sua coluna que armazena a senha com CHAR(60) ou VARCHAR(60), claro se você estiver usando o MySQL. ;)
     *
     * @param  $password
@@ -41,19 +49,19 @@ class GrifoHash
     *
     **/
     public static function make_password($password='')
-    {	
+    {
     	// Gero a string que sera usada pelo metodo crypt
    		$strig_pass = self::makeStringPass($password);
    		// Defino uma seqüência de hash para ser usada pelo crypt no padrao Blowfish
    		$_hashbase = sprintf('$%s$%02d$%s$', self::$Prefix_salt, self::$Cost_default, self::hashBase64('', true));
-   		// Encripta 
+   		// Encripta
    		return crypt($strig_pass, $_hashbase);
 
     }
 
     /**
-    * Método responsável por comparar e validar o hashing da senha. 
-    * Já que estamos trabalhando com um hashing gerado aleatoriamente, seria impossível 
+    * Método responsável por comparar e validar o hashing da senha.
+    * Já que estamos trabalhando com um hashing gerado aleatoriamente, seria impossível
     * gerar um novo hash indentico ao hash que está no banco. mas o método bcrypt/blowfish permite que eu consiga validar a senha.
     *
     * @param  $password, $passwordDB
@@ -100,16 +108,16 @@ class GrifoHash
     {
     	// Retorno a senha codificada pelo metodo hashBase64
    		$Base64 = self::hashBase64($password);
-   		// Converto a senha para uma hash md5 
+   		// Converto a senha para uma hash md5
    		$_md5 = md5($Base64);
    		// Adciono mais uma camada agora utilizando o metodo hash com o padrao sha256
    		$_hash256 = hash('sha256',$_md5);
    		// Adciono mais uma camada agora utilizando o metodo hash com o padrao sha512
    		$_hash512 = hash('sha512',$_hash256);
    		// Inverto o hash criado e contateno com o token utilizado pela classe
-   		return strrev($_hash512).self::$_token;	
+   		return strrev($_hash512).self::$_token;
     }
 
-    
+
 
 }
